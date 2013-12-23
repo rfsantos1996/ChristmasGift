df60645ff9066abb70827689744325832c7e0894
Jabyftw <rfsantos1996@globo.com>
2013-12-23 00:02:07 -0200

    v0.1 - File uploaded (';

--- src/com/jabyftw/christmasg/ChristmasGift.java
+++ src/com/jabyftw/christmasg/ChristmasGift.java
@@ -1,8 +1,5 @@
 package com.jabyftw.christmasg;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
