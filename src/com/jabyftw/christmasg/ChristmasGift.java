aa80c81055531d6e0011c4d2bdcfb25e11b6ddb9
Jabyftw <rfsantos1996@globo.com>
2013-12-22 23:19:18 -0200

    v0.1 - Done for testing

---
+++ src/com/jabyftw/christmasg/ChristmasGift.java
@@ -0,0 +1,167 @@
+package com.jabyftw.christmasg;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.logging.Level;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Rafael
+ */
+public class ChristmasGift extends JavaPlugin implements CommandExecutor {
+
+    private FileConfiguration config;
+    private int giftDelay, grinchChance;
+    private RandomCollection<ItemStack> gifts = new RandomCollection();
+    private Map<String, Map<Integer, ItemStack>> remaining = new HashMap();
+
+    @Override
+    public void onEnable() {
+        // CONFIGURATION
+        config = getConfig();
+        config.addDefault("config.repeatDelayInHour", 24);
+        String[] gift = {"0.8;diamond;10", "0.2;diamond_sword;5"};
+        config.addDefault("config.gifts", Arrays.asList(gift));
+        config.addDefault("config.grinchPercent", 5);
+        config.addDefault("cooldown.test", System.currentTimeMillis());
+        //config.addDefault("lang.", "&");
+        config.addDefault("lang.grinchMessage", "&4Haha, Grinch got you! &cYou lost your gift. ;(");
+        config.addDefault("lang.giftMessage", "&eCongratulations! &6You received a gift!");
+        config.addDefault("lang.noPermission", "&cYou dont have permission!");
+        config.addDefault("lang.onCooldown", "&cYou can only use this every %cooldown hours");
+        config.addDefault("lang.useCommandToGetItems", "&6There are itens remaining! Use &e/christmas remaining &6to get them.");
+        config.addDefault("lang.thereAreNoItensRemaining", "&cThere are no itens remaining!");
+        config.options().copyDefaults(true);
+        saveConfig();
+        reloadConfig();
+        for (Map.Entry<Double, ItemStack> set : getItemStackFromString(config.getStringList("config.gifts")).entrySet()) {
+            gifts.add(set.getKey(), set.getValue());
+        }
+        giftDelay = config.getInt("config.repeatDelayInHour");
+        grinchChance = config.getInt("config.grinchPercent");
+        // PLUGIN
+        getServer().getPluginCommand("christmas").setExecutor(this);
+        getLogger().log(Level.INFO, "Time in ms = {0}", System.currentTimeMillis());
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().log(Level.INFO, "Thanks for using ChristmasGift by rfsantos1996!");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (args.length > 0) {
+            if (sender instanceof Player) {
+                if (sender.hasPermission("christmas.get")) {
+                    Player p = (Player) sender;
+                    String name = p.getName().toLowerCase();
+                    if (remaining.containsKey(name)) {
+                        giveRemaining(p);
+                        return true;
+                    } else {
+                        sender.sendMessage(getLang("thereAreNoItensRemaining"));
+                        return true;
+                    }
+                } else {
+                    sender.sendMessage(getLang("noPermission"));
+                    return true;
+                }
+            } else {
+                sender.sendMessage("Not avaliable on console");
+                return true;
+            }
+        } else {
+            if (sender instanceof Player) {
+                if (sender.hasPermission("christmas.get")) {
+                    Player p = (Player) sender;
+                    String name = p.getName().toLowerCase();
+                    long timeUsed = config.getLong("cooldown." + name);
+                    if (timeUsed > 0) { // have cooldown time
+                        if (outOfCooldown(timeUsed)) {
+                            giveGift(p);
+                            return true;
+                        } else {
+                            sender.sendMessage(getLang("onCooldown").replaceAll("%cooldown", Integer.toString(giftDelay)));
+                            return true;
+                        }
+                    } else { // first time
+                        giveGift(p);
+                        return true;
+                    }
+                } else {
+                    sender.sendMessage(getLang("noPermission"));
+                    return true;
+                }
+            } else {
+                sender.sendMessage("Not avaliable on console");
+                return true;
+            }
+        }
+    }
+
+    private String getLang(String path) {
+        return config.getString("lang." + path).replaceAll("&", "§");
+    }
+
+    private Map<Double, ItemStack> getItemStackFromString(List<String> s) {
+        Map<Double, ItemStack> map = new HashMap();
+        for (String st : s) {
+            String[] st2 = st.split(";");
+            for (Material m : Material.values()) {
+                if (m.toString().equalsIgnoreCase(st2[1])) {
+                    map.put(Double.parseDouble(st2[0]), new ItemStack(m, Integer.parseInt(st2[2])));
+                }
+            }
+            getLogger().log(Level.WARNING, "Couldn''t determinate material for {0}, returning air.", st2[1]);
+        }
+        return map;
+    }
+
+    private boolean takePresent(int chance) {
+        Random r = new Random();
+        return (r.nextInt(100) < chance);
+    }
+
+    private void giveGift(Player p) {
+        if (!takePresent(grinchChance)) {
+            Map<Integer, ItemStack> it = p.getInventory().addItem(gifts.next());
+            if (it.size() > 0) {
+                remaining.put(p.getName().toLowerCase(), it);
+                p.sendMessage(getLang("useCommandToGetItems"));
+            }
+            p.sendMessage(getLang("giftMessage"));
+        } else {
+            p.sendMessage(getLang("grinchMessage"));
+        }
+        config.set("cooldown." + p.getName().toLowerCase(), System.currentTimeMillis());
+        saveConfig();
+    }
+
+    private void giveRemaining(Player p) {
+        Map<Integer, ItemStack> it = p.getInventory().addItem(gifts.next());
+        if (it.size() > 0) {
+            remaining.put(p.getName().toLowerCase(), it);
+            p.sendMessage(getLang("useCommandToGetItems"));
+        } else {
+            remaining.remove(p.getName().toLowerCase());
+        }
+        p.sendMessage(getLang("giftMessage"));
+    }
+
+    private boolean outOfCooldown(long timeUsed) {
+        return System.currentTimeMillis() - timeUsed > (giftDelay * 3600 * 1000);
+    }
+}
